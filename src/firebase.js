921c795a75b1ccb65b24dcdea4f772a46cfd7661
clarasaenzv10-prog <clarasaenzv10@gmail.com>
2026-05-08 17:43:30 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  doc,
-  getDocs,
-  setDoc,
-  deleteDoc,
-  onSnapshot,
-  writeBatch,
-} from "firebase/firestore";
- 
+import { getFirestore, collection, doc, getDocs, setDoc, deleteDoc, onSnapshot, writeBatch } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBfF3r9X9END2GH8mFwDsXmoy7crk0LJiE",
   authDomain: "consultorio-gv-dc2db.firebaseapp.com",
@@ -18,30 +9,21 @@ const firebaseConfig = {
   messagingSenderId: "816478423904",
   appId: "1:816478423904:web:9252d5b442048f3d882c05"
 };
- 
+
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
- 
+
 export function listenCol(name, cb) {
   return onSnapshot(collection(db, name), function(snap) {
     cb(snap.docs.map(function(d) { return Object.assign({}, d.data(), { id: d.id }); }));
   });
 }
- 
-export function saveDoc(col, id, data) {
-  return setDoc(doc(db, col, String(id)), data, { merge: true });
-}
- 
-export function delDoc(col, id) {
-  return deleteDoc(doc(db, col, String(id)));
-}
- 
+export function saveDoc(col, id, data) { return setDoc(doc(db, col, String(id)), data, { merge: true }); }
+export function delDoc(col, id) { return deleteDoc(doc(db, col, String(id))); }
 export async function seedIfEmpty(col, items) {
   const snap = await getDocs(collection(db, col));
   if (!snap.empty) return;
   const batch = writeBatch(db);
-  items.forEach(function(item) {
-    batch.set(doc(db, col, String(item.id)), item);
-  });
+  items.forEach(function(item) { batch.set(doc(db, col, String(item.id)), item); });
   await batch.commit();
 }
